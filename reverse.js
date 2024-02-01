7c4d857b4ea53cf4cd61c3038a5b0a323cc20ad6
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 16:53:08 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,9 @@
+const nums = [4,5,6,9,6,3,1,4,1,2,3,4,5,6];
+
+console.log(nums);
+
+nums.reverse();
+const reversed = nums.reverse(); //same thing in different ways
+
+console.log(reversed);
+// console.log(nums); // we will get the same output as the before console.
