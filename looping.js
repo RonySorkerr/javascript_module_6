16049bdbc077599b611493b088371fcfb0da69d1
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-01 16:48:30 +0600

    looping

---
+++ looping.js
@@ -0,0 +1,10 @@
+const names = ['rahat', 'kamal', 'jamal', 'nabir'];
+for (const name of names) {
+    console.log(name);
+}
+
+for (let i = 0; i < names.length; i++) {
+    console.log(i);
+    console.log('---', names[i])
+
+}
